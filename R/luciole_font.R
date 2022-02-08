5a890b656a230751771d0394e287e50dd70aa93e
pvictor <perrier.victor@gmail.com>
2022-02-08 19:07:07 +0100

    updated luciole_font_dependency()

--- R/luciole_font.R
+++ R/luciole_font.R
@@ -1,5 +1,8 @@
 
-#' @title Integrate Luciole typeface in web pages
+#' @title HTML dependency for Luciole typeface
+#' 
+#' @description It can be used to integrate Luciole font
+#'  into web pages (Shiny applications or RMarkdown documents).
 #'
 #' @param selector CSS selector for which to use the font,
 #'  usually an HTML tag, default to `"body"` (all document).
@@ -11,11 +14,13 @@
 #' @importFrom htmltools tags htmlDependency
 #' @importFrom utils packageVersion
 #'
-#' @example examples/luciole_font.R
+#' @example examples/luciole_font_dependency.R
 luciole_font_dependency <- function(selector = "body") {
   if (!is.null(selector)) {
     css <- paste(selector, "{ font-family: 'Luciole', sans-serif !important; }")
     css <- as.character(tags$style(css))
+  } else {
+    css <- NULL
   }
   htmlDependency(
     name = "luciole",
